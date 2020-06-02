818658fbe713a5df36f5ebe7ecbb4e52fb4bbd42
ascutariu <andreiscutariu92@gmail.com>
2020-06-02 17:24:11 +0300

    updates

--- Week01/01ConsoleApp/Program.cs
+++ Week01/01ConsoleApp/Program.cs
@@ -6,7 +6,17 @@ namespace _01ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int min = 1;
+
+            decimal a, b, c, d;
+
+            min = min + 1;
+
+            Console.WriteLine(min);
+
+            Console.ReadLine();
+
+            a = 12321321321321321321321m;
         }
     }
 }
